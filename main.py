11f9fdbd8ad31758d69181b112cb40c0879a6bae
ilsha <ilshat666.93@gmail.com>
2024-11-11 20:13:56 +0500

    Добавлен интерфейс(метка и кнопка)

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import datetime, time
+# import time
+import pygame
+
+
+def sett():
+    pass
+
+
+window = Tk()
+window.title('Напоминание со звуком')
+
+label = Label(text='Установите напоминание', font=('Courier', 20))
+label.pack(pady=20)
+
+button = Button(text='Установить', font=('Courier', 20), command=sett)
+button.pack(pady=20)
+
+window.mainloop()
